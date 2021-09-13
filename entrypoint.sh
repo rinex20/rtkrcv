7a7c1b65cb1ed3642adb0c49124cc9a07ae28be6
rinex20 <rinex20@me.com>
2021-09-13 15:24:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,7 @@ else
   if [ $ntripcaster -eq 1 ]; then
     /usr/local/bin/ntripcaster /etc/ntripcaster/config.json
   fi
+  if [ $AUTORUN -eq 1 ]; then
   /usr/local/bin/rtkrcv -p 8077 -m 8078 -o /etc/rtk/rtkrcv.conf
+  fi
 fi
