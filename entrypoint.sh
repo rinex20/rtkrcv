87f64d003d8e92f56b9e2d74f4509113c7a85a73
rinex20 <rinex20@me.com>
2021-09-13 16:13:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 if [ -f "/data/rtk/entrypoint.sh" ]; then
+  echo "prepare for the local entrypoint file..."
   chmod a+x /data/rtk/entrypoint.sh
+  echo "start exec local entrypoint file."
   /data/rtk/entrypoint.sh
 else 
+  echo "start entrypoint from build-in entrypoint."
   if [ $ntripcaster -eq 1 ]; then
-    /usr/local/bin/ntripcaster /etc/ntripcaster/config.json
+    echo "ntripcaster started."
+    /usr/local/bin/ntripcaster /etc/ntripcaster/config.json > /dev/null &
   fi
   if [ $AUTORUN -eq 1 ]; then
-  /usr/local/bin/rtkrcv -p 8077 -m 8078 -o /etc/rtk/rtkrcv.conf
+    echo "start rtkrcv..."
+    /usr/local/bin/rtkrcv -p 8077 -m 8078 -o /etc/rtk/rtkrcv.conf
   fi
 fi
