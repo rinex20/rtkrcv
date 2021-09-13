a84a5333968523b1aec6e96dc42e68594c136b69
rinex20 <rinex20@me.com>
2021-09-13 14:42:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ -f "/data/rtk/entrypoint.sh" ]; then
 chmod a+x /data/rtk/entrypoint.sh
 /data/rtk/entrypoint.sh
 else 
-/usr/local/bin/ntripcaster /data/rtk/config.json
-/usr/local/bin/rtkrcv -p 8077 -m 8078 -o /data/rtk/rtkrcv.conf
+/usr/local/bin/ntripcaster /etc/ntripcaster/config.json
+/usr/local/bin/rtkrcv -p 8077 -m 8078 -o /etc/rtk/rtkrcv.conf
 fi
