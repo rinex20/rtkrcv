770aef6d6fe537c0523d96bd8bc52f75fc15aa36
rinex20 <rinex20@me.com>
2022-11-15 13:16:19 +0800

    Update str2str.sh

--- str2str.sh
+++ str2str.sh
@@ -3,13 +3,18 @@
 PID="/home/linaro/str2str_6799.pid"
 EXEC="/usr/local/bin/str2str"
 BIN="busybox start-stop-daemon"
+DEV="ttyUSB0"
 
-if [ "$1" = "start" ];then
-  $BIN -x $EXEC -b -m -p $PID --start -- -in serial://ttyUSB0:115200 -out tcpsvr://:6799 -b 1
-elif [ "$1" = "stop" ];then
-  $BIN -p $PID -K
+if [ -e "/dev/${DEV}" ];then
+  if [ "$1" = "start" ];then
+    $BIN -x $EXEC -b -m -p $PID --start -- -in serial://ttyUSB0:115200 -out tcpsvr://:6799 -b 1
+  elif [ "$1" = "stop" ];then
+    $BIN -p $PID -K
+  else
+    echo "arg is not valid."
+  fi
 else
-  echo "arg is not valid."
+  echo "device not available"
 fi
 
                                                                                                                                                                                                                       13,0-1        All
